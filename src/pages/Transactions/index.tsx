fd58760bd6b57e1d348e937706c63e32bf3c6df1
alissondiniz20 <alisson.dev22@gmail.com>
2023-02-24 12:30:43 -0300

    Tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighLight variant="outcome">- R$ 59,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
